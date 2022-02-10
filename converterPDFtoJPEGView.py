d6eb1bd762264b2a114ba8dac680bf519040039a
Ant Desf <a.desfaits@orange.fr>
2022-02-10 16:12:27 +0100

    interface v1

---
+++ converterPDFtoJPEGView.py
@@ -0,0 +1,96 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys
+
+class Ui_MainWindow(object):
+    def __init__(self):
+        self.core = None
+
+    def setupUi(self, MainWindow) :
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1000, 246)
+        self.initComponents(MainWindow)
+        self.initLayout(MainWindow)
+        self.initConnect()
+
+    def initComponents(self, MainWindow) :
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.destLabel = QtWidgets.QLabel("Destination Folder")
+        self.destEdit = QtWidgets.QLineEdit()
+
+        self.indexBeginLabel = QtWidgets.QLabel("index begin")
+        self.indexEndLabel = QtWidgets.QLabel("index end")
+        self.destPrefixLabel = QtWidgets.QLabel("Image Prefix:")
+        self.destPrefixInput = QtWidgets.QLineEdit()
+        self.destIndexBegin = QtWidgets.QSpinBox()
+        self.destIndexBegin.setMaximum(999999)
+        self.destIndexEnd = QtWidgets.QSpinBox()
+        self.destIndexEnd.setMaximum(999999)
+
+        self.separator = QtWidgets.QWidget()
+        self.separator.setStyleSheet("background-color:black;")
+
+        self.fromLabel = QtWidgets.QLabel("From Folder")
+        self.fromEdit = QtWidgets.QLineEdit()
+
+        self.fromPrefixLabel = QtWidgets.QLabel("Image Prefix:")
+        self.fromindexBeginLabel = QtWidgets.QLabel("index begin")
+        self.fromPrefixInput = QtWidgets.QLineEdit()
+        self.fromIndexBegin = QtWidgets.QSpinBox()
+        self.fromIndexBegin.setMaximum(999999)
+
+        self.separator2 = QtWidgets.QWidget()
+        self.separator2.setStyleSheet("background-color:black;")
+
+        self.convertButton = QtWidgets.QPushButton("Convert")
+
+    def initLayout(self, MainWindow) :
+        self.vbox = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.vbox.addWidget(self.destLabel)
+        self.vbox.addWidget(self.fromLabel)
+        self.hbox1 = QtWidgets.QHBoxLayout()
+        self.hbox2 = QtWidgets.QHBoxLayout()
+        self.hbox3 = QtWidgets.QHBoxLayout()
+        self.hbox4 = QtWidgets.QHBoxLayout()
+
+        self.hbox1.addWidget(self.destLabel)
+        self.hbox1.addWidget(self.destEdit)
+
+        self.hbox2.addWidget(self.destPrefixLabel)
+        self.hbox2.addWidget(self.destPrefixInput)
+        self.hbox2.addWidget(self.indexBeginLabel)
+        self.hbox2.addWidget(self.destIndexBegin)
+        self.hbox2.addWidget(self.indexEndLabel)
+        self.hbox2.addWidget(self.destIndexEnd)
+
+        self.hbox3.addWidget(self.fromLabel)
+        self.hbox3.addWidget(self.fromEdit)
+
+        self.hbox4.addWidget(self.fromPrefixLabel)
+        self.hbox4.addWidget(self.fromPrefixInput)
+        self.hbox4.addWidget(self.fromindexBeginLabel)
+        self.hbox4.addWidget(self.fromIndexBegin)
+
+        self.vbox.addLayout(self.hbox1)
+        self.vbox.addLayout(self.hbox2)
+        self.vbox.addWidget(self.separator)
+        self.vbox.addLayout(self.hbox3)
+        self.vbox.addLayout(self.hbox4)
+        self.vbox.addWidget(self.separator2)
+        self.vbox.addWidget(self.convertButton)
+
+
+    def initConnect(self) :
+        pass
+
+
+
+if __name__ == "__main__" :
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())

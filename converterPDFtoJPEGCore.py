5a3072a7bf1ac826613109bb57faad5eac166900
Ant Desf <a.desfaits@orange.fr>
2022-02-15 18:09:09 +0100

    add final features

--- converterPDFtoJPEGCore.py
+++ converterPDFtoJPEGCore.py
@@ -1,51 +1,89 @@
 from PyQt5 import QtWidgets
+from wand.image import Image as wi
 import converterPDFtoJPEGView
 import sys
+import os
 
 class ConverterPDFtoJPEGCore:
 
     def __init__(self) :
         self.view = None
-        self.destFolder = ""
         self.fromFolder = ""
-        self.destPrefix = ""
+        self.destFolder = ""
         self.fromPrefix = ""
-        self.destIndexBegin = 0
-        self.destIndexEnd = 0
+        self.destPrefix = ""
         self.fromIndexBegin = 0
+        self.fromIndexEnd = 0
+        self.destIndexBegin = 0
+        self.pdfs = []
 
     def setView(self, view):
         self.view = view
 
-    def sendDestFolder(self, path) :
-        self.destFolder = path
-
-    def sendFromFolder(self, path) :
+    def sendfromFolder(self, path) :
         self.fromFolder = path
 
-    def sendPrefix(self, destPrefix, fromPrefix) :
-        self.destPrefix = destPrefix
+    def senddestFolder(self, path) :
+        self.destFolder = path
+
+    def sendPrefix(self, fromPrefix, destPrefix) :
         self.fromPrefix = fromPrefix
+        self.destPrefix = destPrefix
 
-    def sendDestIndexBegin(self, index) :
-        self.destIndexBegin = index
-        self.refreshDestIndex()
+    def sendfromIndexBegin(self, index) :
+        self.fromIndexBegin = int(index)
+        self.refreshfromIndex()
 
-    def sendDestIndexEnd(self, index) :
-        self.destIndexEnd = index
-        self.refreshDestIndex()
+    def sendfromIndexEnd(self, index) :
+        self.fromIndexEnd = int(index)
+        self.refreshfromIndex()
 
-    def sendFromIndexBegin(self, index) :
-        self.fromIndexBegin = index
+    def senddestIndexBegin(self, index) :
+        self.destIndexBegin = int(index)
 
-    def refreshDestIndex(self) :
-        if self.destIndexBegin == 0 : self.view.refreshIndexDest(self.destIndexBegin, self.destIndexEnd)
-        elif self.destIndexBegin > self.destIndexEnd: self.view.refreshIndexDest(self.destIndexBegin, self.destIndexBegin+1)
+    def refreshfromIndex(self) :
+        if self.fromIndexBegin == 0 : self.view.refreshIndexfrom(self.fromIndexBegin, self.fromIndexEnd)
+        elif self.fromIndexBegin > self.fromIndexEnd: self.view.refreshIndexfrom(self.fromIndexBegin, self.fromIndexBegin+1)
 
     def checkAllData(self) :
-        if self.destFolder == "" : self.view.showAlert("Destination Folder is Missiong")
-        elif self.fromFolder == "" : self.view.showAlert("From Folder is Missiong")
+        if self.fromFolder == "" : self.view.showAlert("from Folder is Missing")
+        elif self.destFolder == "" : self.view.showAlert("dest Folder is Missing")
+        else  : self.startConvertion()
 
+    def startConvertion(self) :
+        self.importPDF()
+
+    def importPDF(self) :
+        dir = os.listdir(self.fromFolder)
+        self.view.setValueProgressBar(0,100)
+
+        if self.fromIndexBegin == self.fromIndexEnd : # si les index de début et de fin sont les memes, importer tous les pdf
+            for i, file in enumerate(dir,1) :
+                if file[:len(self.fromPrefix)] == self.fromPrefix and file[-4:] == ".pdf" :
+                    self.pdfs.append(wi(filename=self.fromFolder+"\\"+file, resolution=300).convert("jpeg"))
+                    self.view.setValueProgressBar(i,len(dir))
+        else :
+            for i in range(self.fromIndexBegin, self.fromIndexEnd+1) :
+                if self.fromPrefix+self.reIndex(i)+".pdf" not in dir :
+                    self.view.showAlert(self.fromPrefix+self.reIndex(i)+".pdf is missing")
+                    return -1
+            for i in range(self.fromIndexBegin, self.fromIndexEnd+1) :
+                self.pdfs.append(wi(filename=self.fromFolder+"\\"+self.fromPrefix+self.reIndex(i)+".pdf").convert("jpeg"))
+
+        i = self.destIndexBegin
+        for pdf in self.pdfs:
+            for img  in pdf.sequence :
+                page = wi(image=img)
+                page.save(filename=self.destFolder+"\\"+self.destPrefix+self.reIndex(i)+".jpg")
+                i +=1
+        self.pdfs = []
+
+
+    def reIndex(self, i) :
+        index = str(i)
+        while len(index) < 4:
+            index = "0"+index
+        return index
 
 if __name__ == "__main__" :
     app = QtWidgets.QApplication(sys.argv)
--- converterPDFtoJPEGView.py
+++ converterPDFtoJPEGView.py
@@ -22,63 +22,65 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         MainWindow.setCentralWidget(self.centralwidget)
 
-        self.destLabel = QtWidgets.QLabel("Destination Folder")
-        self.destEdit = QtWidgets.QLineEdit()
-        self.destEdit.setReadOnly(True)
-
-        self.indexBeginLabel = QtWidgets.QLabel("index begin")
-        self.indexEndLabel = QtWidgets.QLabel("index end")
-        self.destPrefixLabel = QtWidgets.QLabel("Image Prefix:")
-        self.destPrefixInput = QtWidgets.QLineEdit()
-        self.destIndexBegin = QtWidgets.QSpinBox()
-        self.destIndexBegin.setMaximum(999999)
-        self.destIndexEnd = QtWidgets.QSpinBox()
-        self.destIndexEnd.setMaximum(999999)
-
-        self.separator = QtWidgets.QWidget()
-        self.separator.setStyleSheet("background-color:black;")
-
         self.fromLabel = QtWidgets.QLabel("From Folder")
         self.fromEdit = QtWidgets.QLineEdit()
         self.fromEdit.setReadOnly(True)
 
+        self.indexBeginLabel = QtWidgets.QLabel("index begin")
+        self.indexEndLabel = QtWidgets.QLabel("index end")
         self.fromPrefixLabel = QtWidgets.QLabel("Image Prefix:")
-        self.fromindexBeginLabel = QtWidgets.QLabel("index begin")
         self.fromPrefixInput = QtWidgets.QLineEdit()
         self.fromIndexBegin = QtWidgets.QSpinBox()
         self.fromIndexBegin.setMaximum(999999)
+        self.fromIndexEnd = QtWidgets.QSpinBox()
+        self.fromIndexEnd.setMaximum(999999)
+
+        self.separator = QtWidgets.QWidget()
+        self.separator.setStyleSheet("background-color:black;")
+
+        self.destLabel = QtWidgets.QLabel("Destination Folder")
+        self.destEdit = QtWidgets.QLineEdit()
+        self.destEdit.setReadOnly(True)
+
+        self.destPrefixLabel = QtWidgets.QLabel("Image Prefix:")
+        self.destindexBeginLabel = QtWidgets.QLabel("index begin")
+        self.destPrefixInput = QtWidgets.QLineEdit()
+        self.destIndexBegin = QtWidgets.QSpinBox()
+        self.destIndexBegin.setMaximum(999999)
 
         self.separator2 = QtWidgets.QWidget()
         self.separator2.setStyleSheet("background-color:black;")
 
         self.convertButton = QtWidgets.QPushButton("Convert")
+        self.progress = QtWidgets.QProgressBar()
+        self.progress.setAlignment(QtCore.Qt.AlignCenter)
 
     def initLayout(self, MainWindow) :
         self.vbox = QtWidgets.QVBoxLayout(self.centralwidget)
-        self.vbox.addWidget(self.destLabel)
         self.vbox.addWidget(self.fromLabel)
+        self.vbox.addWidget(self.destLabel)
         self.hbox1 = QtWidgets.QHBoxLayout()
         self.hbox2 = QtWidgets.QHBoxLayout()
         self.hbox3 = QtWidgets.QHBoxLayout()
         self.hbox4 = QtWidgets.QHBoxLayout()
 
-        self.hbox1.addWidget(self.destLabel)
-        self.hbox1.addWidget(self.destEdit)
+        self.hbox1.addWidget(self.fromLabel)
+        self.hbox1.addWidget(self.fromEdit)
 
-        self.hbox2.addWidget(self.destPrefixLabel)
-        self.hbox2.addWidget(self.destPrefixInput)
+        self.hbox2.addWidget(self.fromPrefixLabel)
+        self.hbox2.addWidget(self.fromPrefixInput)
         self.hbox2.addWidget(self.indexBeginLabel)
-        self.hbox2.addWidget(self.destIndexBegin)
+        self.hbox2.addWidget(self.fromIndexBegin)
         self.hbox2.addWidget(self.indexEndLabel)
-        self.hbox2.addWidget(self.destIndexEnd)
+        self.hbox2.addWidget(self.fromIndexEnd)
 
-        self.hbox3.addWidget(self.fromLabel)
-        self.hbox3.addWidget(self.fromEdit)
+        self.hbox3.addWidget(self.destLabel)
+        self.hbox3.addWidget(self.destEdit)
 
-        self.hbox4.addWidget(self.fromPrefixLabel)
-        self.hbox4.addWidget(self.fromPrefixInput)
-        self.hbox4.addWidget(self.fromindexBeginLabel)
-        self.hbox4.addWidget(self.fromIndexBegin)
+        self.hbox4.addWidget(self.destPrefixLabel)
+        self.hbox4.addWidget(self.destPrefixInput)
+        self.hbox4.addWidget(self.destindexBeginLabel)
+        self.hbox4.addWidget(self.destIndexBegin)
 
         self.vbox.addLayout(self.hbox1)
         self.vbox.addLayout(self.hbox2)
@@ -87,39 +89,45 @@ class Ui_MainWindow(object):
         self.vbox.addLayout(self.hbox4)
         self.vbox.addWidget(self.separator2)
         self.vbox.addWidget(self.convertButton)
+        self.vbox.addWidget(self.progress)
 
 
     def initConnect(self) :
-        self.destEdit.selectionChanged.connect(self.openFolderDest)
-        self.fromEdit.selectionChanged.connect(self.openFolderFrom)
+        self.fromEdit.selectionChanged.connect(self.openFolderfrom)
+        self.destEdit.selectionChanged.connect(self.openFolderdest)
         self.convertButton.clicked.connect(self.tapOnConvertButton)
-        self.destIndexBegin.valueChanged.connect(self.destIndexBeginChanged)
-        self.destIndexEnd.valueChanged.connect(self.destIndexEndChanged)
         self.fromIndexBegin.valueChanged.connect(self.fromIndexBeginChanged)
+        self.fromIndexEnd.valueChanged.connect(self.fromIndexEndChanged)
+        self.destIndexBegin.valueChanged.connect(self.destIndexBeginChanged)
+        self.destPrefixInput.textChanged.connect(self.refreshPrefix)
+        self.fromPrefixInput.textChanged.connect(self.refreshPrefix)
 
-    def openFolderDest(self) :
-        fname = QtWidgets.QFileDialog.getExistingDirectory()
-        if fname :
-            self.destEdit.setText(fname)
-            self.core.sendDestFolder(fname)
-
-    def openFolderFrom(self) :
+    def openFolderfrom(self) :
         fname = QtWidgets.QFileDialog.getExistingDirectory()
         if fname :
             self.fromEdit.setText(fname)
-            self.core.sendFromFolder(fname)
+            self.core.sendfromFolder(fname)
 
-    def destIndexBeginChanged(self, value) :
-        self.core.sendDestIndexBegin(value)
-
-    def destIndexEndChanged(self, value) :
-        self.core.sendDestIndexEnd(value)
+    def openFolderdest(self) :
+        fname = QtWidgets.QFileDialog.getExistingDirectory()
+        if fname :
+            self.destEdit.setText(fname)
+            self.core.senddestFolder(fname)
 
     def fromIndexBeginChanged(self, value) :
-        self.core.sendFromIndexBegin(value)
+        self.core.sendfromIndexBegin(value)
 
-    def refreshIndexDest(self, indexBegin, indexEnd) :
-        self.destIndexEnd.setValue(indexEnd)
+    def fromIndexEndChanged(self, value) :
+        self.core.sendfromIndexEnd(value)
+
+    def destIndexBeginChanged(self, value) :
+        self.core.senddestIndexBegin(value)
+
+    def refreshIndexfrom(self, indexBegin, indexEnd) :
+        self.fromIndexEnd.setValue(indexEnd)
+
+    def refreshPrefix(self) :
+        self.core.sendPrefix(self.fromPrefixInput.text(), self.destPrefixInput.text())
 
     def tapOnConvertButton(self) :
         self.core.checkAllData()
@@ -128,6 +136,9 @@ class Ui_MainWindow(object):
         u_dialog = QtWidgets.QDialog()
         dialog = converterPDFtoJPEGAlertDialog.ConverterPDFtoJPEGAlertDialog(u_dialog, message)
         u_dialog.exec_()
+
+    def setValueProgressBar(self, val, max) :
+        self.progress.setValue(val/max*100)
 
 
 if __name__ == "__main__" :
